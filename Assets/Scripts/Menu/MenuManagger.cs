1d32ae15c36434fb1f996acc151523250d99be08
Mon <mon.pereira6029@gmail.com>
2025-05-20 23:12:32 +0200

    build con creditos

--- Assets/Scripts/Menu/MenuManagger.cs
+++ Assets/Scripts/Menu/MenuManagger.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 public class MenuManagger : MonoBehaviour
 {
 
+    public GameObject panelCreditos;
+
      public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void AbrirCreditos()
+    {
+        panelCreditos.SetActive(true);
+    }
+
+    public void CerrarCreditos()
+    {
+        panelCreditos.SetActive(false);
+    }
 
      public void Exit()
     {
